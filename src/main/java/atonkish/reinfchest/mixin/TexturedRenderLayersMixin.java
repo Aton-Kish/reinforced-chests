62e0a050aced136d834ec0f206da33bfcb0c33f9
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-09-19 00:13:26 +0900

    feat: add texture render layers mixin

---
+++ src/main/java/atonkish/reinfchest/mixin/TexturedRenderLayersMixin.java
@@ -0,0 +1,29 @@
+package atonkish.reinfchest.mixin;
+
+import java.util.function.Consumer;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.TexturedRenderLayers;
+import net.minecraft.client.util.SpriteIdentifier;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import atonkish.reinfcore.util.ReinforcingMaterial;
+import atonkish.reinfchest.client.render.ModTexturedRenderLayers;
+
+@Environment(EnvType.CLIENT)
+@Mixin(TexturedRenderLayers.class)
+public class TexturedRenderLayersMixin {
+    @Inject(at = @At("HEAD"), method = "addDefaultTextures")
+    private static void addDefaultTextures(Consumer<SpriteIdentifier> adder, CallbackInfo info) {
+        for (ReinforcingMaterial material : ReinforcingMaterial.values()) {
+            adder.accept(ModTexturedRenderLayers.REINFORCED_CHEST_SINGLE_MAP.get(material));
+            adder.accept(ModTexturedRenderLayers.REINFORCED_CHEST_LEFT_MAP.get(material));
+            adder.accept(ModTexturedRenderLayers.REINFORCED_CHEST_RIGHT_MAP.get(material));
+        }
+    }
+}
