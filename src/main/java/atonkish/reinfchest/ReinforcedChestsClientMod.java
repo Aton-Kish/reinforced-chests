addc41984b3a607dea1179d104b39e0de36d39b8
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-09-18 14:19:22 +0900

    chore: init my mod

---
+++ src/main/java/atonkish/reinfchest/ReinforcedChestsClientMod.java
@@ -0,0 +1,12 @@
+package atonkish.reinfchest;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ReinforcedChestsClientMod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
