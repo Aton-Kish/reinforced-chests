aaeba2bb89af1fa99c676ea3c7dadfcc7beef1ab
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2024-06-02 15:52:09 +0900

    added inventory tests

--- src/gametest/java/atonkish/reinfchest/gametest/ReinforcedChestsModGameTest.java
+++ src/gametest/java/atonkish/reinfchest/gametest/ReinforcedChestsModGameTest.java
@@ -2,6 +2,7 @@ package atonkish.reinfchest.gametest;
 
 public class ReinforcedChestsModGameTest {
     public static final String BATCH_ID_ADVANCEMENT = "advancementBatch";
+    public static final String BATCH_ID_INVENTORY = "inventoryBatch";
     public static final String BATCH_ID_LOOT_TABLE = "lootTableBatch";
     public static final String BATCH_ID_RECIPE = "recipeBatch";
     public static final String BATCH_ID_STAT = "statBatch";
