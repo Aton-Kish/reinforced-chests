b81ea6b6d62cadc356ef1379668f9893092e98e9
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2024-06-16 15:39:53 +0900

    fixed recipe tests

--- src/main/java/atonkish/reinfchest/gametest/testcase/RecipeTests.java
+++ src/main/java/atonkish/reinfchest/gametest/testcase/RecipeTests.java
@@ -2,25 +2,24 @@ package atonkish.reinfchest.gametest.testcase;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 
-import net.minecraft.inventory.CraftingInventory;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.inventory.RecipeInputInventory;
-import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeManager;
 import net.minecraft.recipe.RecipeType;
+import net.minecraft.recipe.input.CraftingRecipeInput;
+import net.minecraft.recipe.input.SmithingRecipeInput;
+import net.minecraft.recipe.input.RecipeInput;
 import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.test.StructureTestUtil;
 import net.minecraft.test.TestFunction;
 
 import atonkish.reinfchest.ReinforcedChestsMod;
-import atonkish.reinfchest.gametest.util.VoidScreenHander;
 import atonkish.reinfchest.item.ModItems;
 import atonkish.reinfcore.util.ReinforcingMaterials;
 
@@ -40,10 +39,10 @@ public class RecipeTests {
                 add(RecipeTests.createTest(
                         "Craft Copper Chest",
                         RecipeType.CRAFTING,
-                        RecipeTests.create3x3CraftingInventory(
+                        CraftingRecipeInput.create(3, 3, List.of(
                                 material, material, material,
                                 material, baseChest, material,
-                                material, material, material),
+                                material, material, material)),
                         chest));
             }
 
@@ -58,10 +57,10 @@ public class RecipeTests {
                 add(RecipeTests.createTest(
                         "Craft Iron Chest",
                         RecipeType.CRAFTING,
-                        RecipeTests.create3x3CraftingInventory(
+                        CraftingRecipeInput.create(3, 3, List.of(
                                 material, material, material,
                                 material, baseChest, material,
-                                material, material, material),
+                                material, material, material)),
                         chest));
             }
 
@@ -76,10 +75,10 @@ public class RecipeTests {
                 add(RecipeTests.createTest(
                         "Craft Gold Chest",
                         RecipeType.CRAFTING,
-                        RecipeTests.create3x3CraftingInventory(
+                        CraftingRecipeInput.create(3, 3, List.of(
                                 material, material, material,
                                 material, baseChest, material,
-                                material, material, material),
+                                material, material, material)),
                         chest));
             }
 
@@ -94,10 +93,10 @@ public class RecipeTests {
                 add(RecipeTests.createTest(
                         "Craft Diamond Chest",
                         RecipeType.CRAFTING,
-                        RecipeTests.create3x3CraftingInventory(
+                        CraftingRecipeInput.create(3, 3, List.of(
                                 material, material, material,
                                 material, baseChest, material,
-                                material, material, material),
+                                material, material, material)),
                         chest));
             }
 
@@ -113,14 +112,14 @@ public class RecipeTests {
                 add(RecipeTests.createTest(
                         "Smithing Netherite Chest",
                         RecipeType.SMITHING,
-                        new SimpleInventory(template, baseChest, material),
+                        new SmithingRecipeInput(template, baseChest, material),
                         chest));
             }
         }
     };
 
-    private static <C extends Inventory, T extends Recipe<C>> TestFunction createTest(String name, RecipeType<T> type,
-            C inventory, ItemStack expected) {
+    private static <I extends RecipeInput, T extends Recipe<I>> TestFunction createTest(String name,
+            RecipeType<T> type, I input, ItemStack expected) {
         String testName = String.format("%s %s %s",
                 ReinforcedChestsMod.MOD_ID,
                 RecipeTests.class.getSimpleName(),
@@ -144,10 +143,10 @@ public class RecipeTests {
                     ServerWorld world = context.getWorld();
                     RecipeManager recipeManager = world.getRecipeManager();
                     DynamicRegistryManager registryManager = world.getRegistryManager();
-                    T recipe = recipeManager.getFirstMatch(type, inventory, world).orElseThrow().value();
+                    T recipe = recipeManager.getFirstMatch(type, input, world).orElseThrow().value();
 
                     // Act
-                    ItemStack actual = recipe.craft(inventory, registryManager);
+                    ItemStack actual = recipe.craft(input, registryManager);
 
                     // Assert
                     try {
@@ -160,22 +159,5 @@ public class RecipeTests {
 
                     context.complete();
                 });
-    }
-
-    private static RecipeInputInventory create3x3CraftingInventory(
-            ItemStack stack1, ItemStack stack2, ItemStack stack3,
-            ItemStack stack4, ItemStack stack5, ItemStack stack6,
-            ItemStack stack7, ItemStack stack8, ItemStack stack9) {
-        RecipeInputInventory inventory = new CraftingInventory(new VoidScreenHander(), 3, 3);
-        inventory.setStack(0, stack1);
-        inventory.setStack(1, stack2);
-        inventory.setStack(2, stack3);
-        inventory.setStack(3, stack4);
-        inventory.setStack(4, stack5);
-        inventory.setStack(5, stack6);
-        inventory.setStack(6, stack7);
-        inventory.setStack(7, stack8);
-        inventory.setStack(8, stack9);
-        return inventory;
     }
 }
