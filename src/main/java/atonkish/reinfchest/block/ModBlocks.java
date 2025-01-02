40af67f0c64a7730ba439374dd28b9db9a2f6a9d
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2025-01-02 20:57:13 +0900

    added registry key for block/items

--- src/main/java/atonkish/reinfchest/block/ModBlocks.java
+++ src/main/java/atonkish/reinfchest/block/ModBlocks.java
@@ -2,10 +2,14 @@ package atonkish.reinfchest.block;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 import atonkish.reinfcore.util.ReinforcingMaterial;
@@ -21,17 +25,29 @@ public class ModBlocks {
         }
 
         if (!REINFORCED_CHEST_MAP.containsKey(material)) {
-            Block block = ModBlocks.register(namespace, material.getName() + "_chest",
-                    new ReinforcedChestBlock(material,
+            Block block = ModBlocks.register(Identifier.of(namespace, material.getName() + "_chest"),
+                    (abstractBlockSettings) -> new ReinforcedChestBlock(material,
                             () -> ModBlockEntityType.REINFORCED_CHEST_MAP.get(material),
-                            REINFORCED_CHEST_SETTINGS_MAP.get(material)));
+                            abstractBlockSettings),
+                    REINFORCED_CHEST_SETTINGS_MAP.get(material));
             REINFORCED_CHEST_MAP.put(material, block);
         }
 
         return REINFORCED_CHEST_MAP.get(material);
     }
 
-    private static Block register(String namespace, String id, Block block) {
-        return Registry.register(Registries.BLOCK, Identifier.of(namespace, id), block);
+    private static Block register(RegistryKey<Block> key, Function<AbstractBlock.Settings, Block> factory,
+            AbstractBlock.Settings settings) {
+        Block block = factory.apply(settings.registryKey(key));
+        return Registry.register(Registries.BLOCK, key, block);
+    }
+
+    private static RegistryKey<Block> keyOf(Identifier id) {
+        return RegistryKey.of(RegistryKeys.BLOCK, id);
+    }
+
+    private static Block register(Identifier id, Function<AbstractBlock.Settings, Block> factory,
+            AbstractBlock.Settings settings) {
+        return register(keyOf(id), factory, settings);
     }
 }
